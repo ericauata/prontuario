95121ecfbb4f08c0ec6a073fe62003cd35464987
ericauata <115047433+ericauata@users.noreply.github.com>
2022-11-12 09:37:13 -0300

    Refactored folder struture to nested

--- index.js
+++ index.js
@@ -82,10 +82,10 @@ import { fileURLToPath } from "url"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-app.use(express.static(path.join(__dirname, "../dist")))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("*", (req, res) => {
-   res.sendFile(path.join(__dirname, "../dist", "index.html"))
+   res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
 const port = process.env.PORT || 5000
